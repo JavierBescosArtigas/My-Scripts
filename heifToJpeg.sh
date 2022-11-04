f73e8263d24b635162494498b247f79140226573
Atomic_Ant <javierbescoss@gmail.com>
2022-11-04 01:43:25 +0100

    me he equivocado y no he hecho comits a mitad y la he liao, al juntar branches he perdido todo el progreso...

--- heifToJpeg.sh
+++ heifToJpeg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#Ejecutable made by AtomicAnt
 #Colours
 
 declare -r greenColour="\e[0;32m\033[1m"
@@ -11,18 +11,28 @@ declare -r purpleColour="\e[0;35m\033[1m"
 declare -r turquoiseColour="\e[0;36m\033[1m"
 declare -r grayColour="\e[0;37m\033[1m"
 
-echo -e "[~]$greenColour Funcionamiento: sitúate en una carpeta con .heic archivos y escriba 'go'; automáticamente transforma los archivos a jpeg y los guarda en una carpeta\n$endColour"
-echo -e "[!]$blueColour si quiere descomprimir primero un .zip, escriba $purpleColour'zip'$blueColour, el zip debe contener los archivos, no carpetas con los archivos\n$endColour"
+function ctrl_c() {
+echo -e "$endColour[!]$redColour Saliendo..., los archivos que ya se han transformado se encuentran en heiftojpeg, si quiere seguir elimine los .heic que se llamen igual a los siguientes, y renombre la carpeta heiftojpeg ó se borrará automáticamente al correr el script otra vez!"
+tput cnorm; exit 1
+}
+
+#ctrl-c
+tput civis
+trap ctrl_c INT
+echo -e "[~]$greenColour Funcionamiento: sitúate en una carpeta con .heic archivos y escriba $purpleColour'go'$greenColour, automáticamente transforma los archivos a jpeg y los guarda en una carpeta\n$endColour"
+echo -e "[~]$greenColour si quiere descomprimir primero un .zip, escriba $purpleColour'zip'$greenColour, el zip debe contener los archivos, no carpetas con los archivos\n$endColour"
 read -t 10 action
-if [[ $action -eq 'zip' ]]; then
-    echo -e "contenido del zip, asegúrese de que no tiene subcarpetas, se va a descomprimir, si tiene subcarpetas donde se encuentran los heif, primero desplácese a ellas y luego ejecute go\n"
-    7z l ./*.zip | tail -n 5
-    echo -e "$redColour[!]Empezando descompresión, esto puede llevar un rato según el nº de archivos...\n"
-    7z e ./*.zip >/dev/null
-    echo -e "$endColour[~]$greenColour Ahora, si no hay subcarpetas escriba 'go' y se procederá a cambiar el formato de todos los .heic de la carpeta\n"
+if [[ $action == 'zip' ]]; then
+    echo -e "[!] $yellowColour Contenido del zip, asegúrese de que no tiene subcarpetas, el script no funciona recursivamente$endColour\n"
+    nombreZip=$(for elto in $(ls *.zip); do echo $elto; break; done)
+    7z l $nombreZip | tail -n 5
+    echo -e "$blueColour[~] Empezando descompresión, esto puede llevar un rato según el nº de archivos...\n"
+    echo -e "$yellowColour[!] Si hay elementos con el mismo nombre dentro del zip y en la carpeta actual, se atascará!"
+    7z e $nombreZip >/dev/null
+    echo -e "$endColour[~]$greenColour Ahora, si no hay subcarpetas escriba $purpleColour'go'$greenColour y se procederá a cambiar el formato de todos los .heic de la carpeta$endColour\n"
     read -t 10 action
 fi
-if [[ $action -eq 'go' ]]; then
+if [[ $action == 'go' ]]; then
     rm -rf ./heifTojpeg >/dev/null
     mkdir heifTojpeg
     counter=$(ls *.heic | wc -l)
@@ -38,20 +48,21 @@ if [[ $action -eq 'go' ]]; then
         done
     else
         echo -e "$redColour[!] $yellowColour Falta instalar libhei-example! sudo apt...\n$endColour"
-        exit 1
+        tput cnorm; exit 1
     fi
 else
-    echo "[X] Saliendo..."
+    echo -e "$endColour[X]$redColour Saliendo..."
+    tput cnorm; exit 0
 fi
 echo -e "$blueColour RESULTADO!\n$endColour"
 ls ./heifTojpeg
 echo Comprimiendo en .zip...
 zip -r -9 jotapeges.zip heifTojpeg
 rm -rf heifTojpeg
-echo -e "[!]$redColour ¿Quiere borrar todos los .heic de la carpeta actual? Escriba $purpleColour 'Si'"
+echo -e "[!]$greenColour ¿Quiere borrar todos los .heic de la carpeta actual? Escriba $purpleColour 'yes'$endColour"
 read -t 300 action
-if [[ $action -eq 'Si' ]]; then
+if [[ $action == 'yes' ]]; then
     rm -rf ./*.heic
 fi
-echo "Adios!!!"; exit 0
+echo "Adios!!!"; tput cnorm; exit 0
 
